050e7a80e241758b51581955d3233d12ce948483
Ben North <ben@redfrontdoor.org>
2015-02-16 21:50:05 +0000

    remove 'print' for n_letters_in_colour
    We're now happy this is working, so get rid of the clutter which appears
    on the screen.

--- fortune.py
+++ fortune.py
@@ -12,7 +12,6 @@ colour_choice = player_choice('colour', outside_colours)
 print('you chose ' + colour_choice)
 
 n_letters_in_colour = len(colour_choice)
-print(n_letters_in_colour)
 
 # RED (3 letters) and GREEN (5 letters) lead to [1, 2, 5, 6]
 # BLUE (4 letters) and YELLOW (6 letters) lead to [3, 4, 7, 8]
